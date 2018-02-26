c5b483571907a1b464938d24784f3b79d26927fa
testykodilla <32685259+testykodilla@users.noreply.github.com>
2018-02-26 19:13:01 +0100

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,42 +1,44 @@
-import uuid from uuid;
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+import uuid from 'uuid';
 
 function addComment(text) {
     return {
         type: ADD_COMMENT,
-        text: text,
+        text,
         id: uuid.v4()
-}};
+    }
+}
 
 function editComment(id, text) {
     return {
         type: EDIT_COMMENT,
-        text: text,
-}};
+        text,
+        id
+    }   
+}
 
 function removeComment(id) {
     return {
         type: REMOVE_COMMENT,
-        id: id,
-}};
+        id
+    }
+}   
 
-function thumbsUp(id, likes) {
+function thumbsUp(id) {
     return {
         type: THUMB_UP_COMMENT,
-        id: id,
-        likes: likes += 1,
+        id
     }
-};
+}
 
-function thumbsDown(id, likes) {
+function thumbsDown(id) {
     return {
         type : THUMB_DOWN_COMMENT,
-        id: id,
-        likes: likes -= 1,
+        id
     }
-};
+}
 
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
